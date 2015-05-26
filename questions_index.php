5f6c163b0eb9efab292142243171f3625d4637e6
tsukayama <tsukayama@syon.co.jp>
2015-05-26 11:12:41 +0900

    質問一覧ページ仮追加

---
+++ questions_index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>くりっかー</title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <style type="text/css">
+body
+{
+    padding-top: 50px;
+}
+        </style>
+        <script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">くりっかー</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="page-header">
+                <h3>質問一覧</h3>
+            </div>
+
+            <table class="table">
+                <tr>
+                    <th>ID</th>
+                    <th>質問名</th>
+                    <th>回答</th>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>りんごの色？</td>
+                    <td>回答ページへ</td>
+                </tr>
+            </table>
+        </div>
+    </body>
+
+</html>
