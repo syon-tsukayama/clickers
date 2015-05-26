ca3462faafc616a6d1bd7dd17abff86874904e19
tsukayama <tsukayama@syon.co.jp>
2015-05-26 10:48:21 +0900

    index.php 追加 jquery 2.1.4 追加 bootsrap 3.3.4 追加

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>くりっかー</title>
+        <link href="./css/bootstrap.min.css" rel="stylesheet">
+        <style type="text/css">
+body
+{
+    padding-top: 50px;
+}
+        </style>
+        <script type="text/javascript" src="./js/jquery-2.1.4.min.js"></script>
+        <script type="text/javascript" src="./js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <a href="#" class="navbar-brand">くりっかー</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
+            <div class="page-header">
+                <h3>テストトップページ</h3>
+            </div>
+        </div>
+    </body>
+
+</html>
