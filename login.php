013ef00f1f12d2724e4bd31bb8e9c0f0a742f570
tsukayama <tsukayama@syon.co.jp>
2015-12-03 15:21:57 +0900

    ログイン処理修正

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+
+// 共通機能読み込み
+require_once('common.php');
+?>
 <html>
 
     <head>
@@ -17,17 +23,47 @@
             <div class="page-header">
                 <h3>ログイン結果</h3>
             </div>
+            <?php
+            // データベース接続処理
+            $conn = connect_database();
 
-            if(isset($row['id']) && is_numeric($row['id']) && $row['id'] > 0)
+            if($conn)
             {
-                $_SESSION['user_id'] = $row['id'];
-                $_SESSION['user_name'] = $row['username'];
+                // 検索SQL
+                $sql =<<<EOS
+SELECT `id`, `username` FROM `users`
+WHERE `username` = :username AND `password` = :password
+EOS;
+
+            $stmt = $conn->prepare($sql);
+            $stmt->bindValue(':username', $_POST['username']);
+            $stmt->bindValue(':password', $_POST['password']);
+
+            $result = $stmt->execute();
+            if($result)
+            {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                if(isset($row['id']) && is_numeric($row['id']) && $row['id'] > 0)
+                {
+                    $_SESSION['user_id'] = $row['id'];
+                    $_SESSION['user_name'] = $row['username'];
                 ?>
                 <div class="alert alert-success" role="alert">
                     <?php echo 'ログイン成功'; ?>
                 </div>
                 <a href="questions_index.php" class="btn btn-default">質問一覧</a>
                 <?php
+                }
+                else
+                {
+                ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo 'ログイン失敗'; ?>
+                </div>
+                <a href="login_form.php" class="btn btn-default">ログインページへ</a>
+                <?php
+                }
             }
             else
             {
@@ -41,7 +77,12 @@
         }
         else
         {
-            echo '接続失敗';
+        ?>
+                <div class="alert alert-danger" role="alert">
+                    <?php echo 'データベース接続失敗'; ?>
+                </div>
+                <a href="login_form.php" class="btn btn-default">ログインページへ</a>
+        <?php
         }
         ?>
                 </div>
