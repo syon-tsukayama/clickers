379774f70db86d27a1dfe0e38dd618aa26665891
tsukayama <tsukayama@syon.co.jp>
2015-12-03 15:21:41 +0900

    ログインフォームソース整形

--- login_form.php
+++ login_form.php
@@ -13,31 +13,33 @@
 
             <div class="row">
                 <div class="col-md-3"></div>
+
                 <div class="col-md-6">
                     <h2>ログイン</h2>
                     <div class="well">
 
-        <form action="login.php" method="post" class="form-horizontal">
-            <div class="form-group">
-                <label class="col-sm-4 control-label">ログイン名：</label>
-                <div class="col-sm-8">
-                    <input type="text" name="username" class="form-control">
-                </div>
-            </div>
-            <div class="form-group">
-                <label class="col-sm-4 control-label">パスワード：</label>
-                <div class="col-sm-8">
-                    <input type="password" name="password" class="form-control">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-4 col-sm-8">
-                    <button type="submit" class="btn btn-default">ログイン</button>
-                </div>
-            </div>
-        </form>
+                        <form action="login.php" method="post" class="form-horizontal">
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label">ログイン名：</label>
+                                <div class="col-sm-8">
+                                    <input type="text" name="username" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label class="col-sm-4 control-label">パスワード：</label>
+                                <div class="col-sm-8">
+                                    <input type="password" name="password" class="form-control">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-4 col-sm-8">
+                                    <button type="submit" class="btn btn-default">ログイン</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
+
                 <div class="col-md-3"></div>
             </div>
         </div>
